847ad03ffad8352252291a3c52decc44a980c28c
Allen Ng <allen.ng@spglobal.com>
2024-04-15 20:01:57 -0400

    Finally added aws creds script

--- aws-creds.sh
+++ aws-creds.sh
@@ -1,15 +1,73 @@
 #!/bin/bash
+# manage ChartIQ SES keys
 
 set -e
-trap "set +e" HUP INT ABRT EXIT QUIT TERM
-if [ -z "$1" ]; then
-	read -p "MFA token: " TOTP_TOKEN
-else
-	TOTP_TOKEN=$1
-fi
-RESP=$(aws sts get-session-token --serial arn:aws:iam::107055233533:mfa/allenng --token-code $TOTP_TOKEN)
-# echo $RESP
-read AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< $(echo $RESP | awk '{print $2" "$4" "$5}')
-# printf $AWS_ACCESS_KEY_ID"\n"$AWS_SECRET_ACCESS_KEY"\n"$AWS_SESSION_TOKEN
-# export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
-printf "[default]\naws_access_key_id = AKIARR3H5HH6QON4C3XE\naws_secret_access_key = 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\n[MFA]\naws_access_key_id = "$AWS_ACCESS_KEY_ID"\naws_secret_access_key = "$AWS_SECRET_ACCESS_KEY"\naws_session_token = "$AWS_SESSION_TOKEN > ~/.aws/credentials
+
+list_keys(){
+	if [ "$#" -ne 1 ]; then
+		echo Please provide the profile name
+		exit 1
+	fi
+	aws iam list-access-keys --user-name chartiq-ses-chartiq.com --profile "$1"
+}
+
+create_key(){
+	if [ "$#" -lt 1 ]; then
+		echo Please provide the name of the profile to use
+		exit 1
+	fi
+	RESP=$(aws iam create-access-key --user-name chartiq-ses-chartiq.com --profile "$1")
+	NEW_PROFILE=ses-$(date +'%Y%m%d')
+	aws configure --profile "$NEW_PROFILE" set aws_access_key_id "$(jq -r '.AccessKey.AccessKeyId' <<< "$RESP")"
+	aws configure --profile "$NEW_PROFILE" set aws_secret_access_key "$(jq -r '.AccessKey.SecretAccessKey' <<< "$RESP")"
+}
+
+update_key(){
+	if [ "$#" -lt 1 ]; then
+		echo Please provide the name of the profile to use
+		exit 1
+	fi
+	if [ "$#" -ne 2 ]; then
+		echo Please provide the name of the profile to modify
+		exit 1
+	fi
+	OLD_KEY=$(aws configure get $2.aws_access_key_id)
+	aws iam update-access-key --access-key-id $OLD_KEY --status Inactive --user-name chartiq-ses-chartiq.com --profile $1
+	# sed -i '' 's/^\['"$1"'\]$/\['"$1"'-old\]/' ~/.aws/*
+}
+
+delete_key(){
+	if [ "$#" -lt 1 ]; then
+		echo Please provide the name of the profile to use
+		exit 1
+	fi
+	if [ "$#" -ne 2 ]; then
+		echo Please provide the name of the profile to modify
+		exit 1
+	fi
+	OLD_KEY=$(aws configure get $2.aws_access_key_id)
+	aws iam delete-access-key --access-key-id $OLD_KEY --user-name chartiq-ses-chartiq.com --profile $1
+}
+
+#commands
+case "$1" in
+	create)
+		create_key $2
+		;;
+	delete)
+		delete_key $2 $3
+		;;
+	list)
+		list_keys $2
+		;;
+	profiles)
+		aws configure list-profiles
+		;;
+	update)
+		update_key $2 $3
+		;;
+	*)
+		echo Available commands:
+		awk '/^[[:space:]]*#commands/{cmdBlock = 1; next}/^[[:space:]]*esac/{cmdBlock = 0}cmdBlock && /^[[:space:]]*[a-zA-Z0-9_]+\)[[:space:]]*$/{sub(/[[:space:]]*\)[[:space:]]*$/, ""); print $1}' $0
+		;;
+esac
